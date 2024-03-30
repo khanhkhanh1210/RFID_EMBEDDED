78874e183f525dccd8feb634a51c4624739484b4
Nguyen Thanh Yen Thy <yenthyngth1133@gmail.com>
2024-03-31 01:22:42 +0700

    date time update

--- check_in_gui/scanID_app.py
+++ check_in_gui/scanID_app.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ui.app_ui import Ui_MainWindow
 import pandas as pd
@@ -9,18 +10,43 @@ class APP(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        # Create a timer
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.scanID)
+        self.timer.start(1000)  # Trigger the scanID function every 5 seconds
     
-    def getdata(self):
-        #Get RFID ID from keyboard
-        ID = self.ui.textEdit.toPlainText()
-        
+    def scanID(self):
+        #Calling a function when the lineEdit field is changed
+        self.ui.lineEdit.textChanged.connect(self.getdatetime)
+        # Get the student ID from the lineEdit field and store it in a variable
+        student_id = self.ui.lineEdit.text()
 
-
-        #Get checked-in time
+    def getdatetime(self):
+        self.ui.lineEdit_3.setEnabled(True)
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        self.ui.textEdit_3.setPlainText(str(current_time))
+        self.ui.lineEdit_3.setText(current_time)
+
+    # Write a function to clear the lineEdit fields when scanning a new student ID
+    def clearID(self):
+        self.ui.lineEdit.clear()  
+
+        # Read the data from the CSV file and check if the student ID is in the file
+        #data = pd.read_csv('attendees.csv')
+        #if student_id in data['Student ID'].values:
+            # Get the student's name and checked-in date from the file
+        #    student_name = data.loc[data['Student ID'] == student_id, 'Student Name'].values[0]
+        #    checked_in_date = data.loc[data['Student ID'] == student_id, 'Checked-in Date'].values[0]
+            # Display the student's name and checked-in date in the lineEdit fields
+        #    self.ui.lineEdit_2.setText(student_name)
+        #    self.ui.lineEdit_3.setText(checked_in_date)
+        #else:
+            # If the student ID is not in the file, display an error message
+        #    self.ui.lineEdit_2.setText("Student not found")
+        #    self.ui.lineEdit_3.setText("N/A")
         
+
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- check_in_gui/test.py
+++ check_in_gui/test.py
@@ -1,6 +1,60 @@
-from datetime import datetime
+# Importing PyQt 
+from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QPushButton 
+import sys 
 
-now = datetime.now()
+class TextEditDemo(QWidget): 
+	def __init__(self, parent=None): 
+		super().__init__(parent) 
+		
+		# Setting the title of the GUI 
+		self.setWindowTitle("QTextEdit") 
+		
+		# Setting the size of the window 
+		self.resize(300, 270) 
+		
+		# Creating the first textedit 
+		self.textEdit1 = QTextEdit() 
+		
+		# Creating the second textedit 
+		self.textEdit2 = QTextEdit() 
+		
+		# Creating the button 
+		self.btnPress1 = QPushButton("Button 1") 
+		
+		# Creating a vertical box layout 
+		layout = QVBoxLayout() 
+		
+		# Adding the first textedit into the layout 
+		layout.addWidget(self.textEdit1) 
+		
+		# Adding the second textedit into the layout 
+		layout.addWidget(self.textEdit2) 
+		
+		# Adding the button into the layout 
+		layout.addWidget(self.btnPress1) 
 
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
+		self.setLayout(layout) 
+
+		# Calling the function when the button is clicked 
+		self.btnPress1.clicked.connect(self.btnPress1_Clicked) 
+
+	def btnPress1_Clicked(self): 
+		''' 
+		This function reads the data from the first TextEdit and 
+		add 10 to it. After adding the number, it displays the result 
+		in the second TextEdid. 
+		'''
+		result = int(self.textEdit1.toPlainText()) + 10
+		
+		# Displaying the result in the second textEdit. 
+		self.textEdit2.setPlainText(str(result)) 
+
+
+if __name__ == '__main__': 
+	''' 
+	Main code that creates the GUI 
+	'''
+	app = QApplication(sys.argv) 
+	win = TextEditDemo() 
+	win.show() 
+	sys.exit(app.exec_()) 
